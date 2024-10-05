965977a166eae6c1990dbe17b14c4d805f2731d6
Computer4062 <mihan.edirisinghe@gmail.com>
2024-10-05 23:53:11 +0400

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import api from "./index.js"
+
+const app = express();
+const port = 3300;
+
+app.use(express.json());
+app.use("/api", api);
+
+app.listen(port, () => {
+	console.log(`🚀 Server is listening on Port ${port}`);
+});
